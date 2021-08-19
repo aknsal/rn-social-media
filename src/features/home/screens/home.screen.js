685bf64198c3b195c6cc9cf028d1afba0847369f
Akshaj <akshajkansal@yahoo.com>
2021-08-20 01:10:21 +0530

    Add FAB in HomeScreen

--- src/features/home/screens/home.screen.js
+++ src/features/home/screens/home.screen.js
@@ -4,12 +4,14 @@ import React from "react";
 import { View, Text, StyleSheet } from "react-native";
 import { Post } from "../components/post.component";
 import { StatusBarView } from "../../../constants/statusBar/StatusBarHeightView.component";
+import FABComponent from "../../../components/FAB";
 
 const HomeScreen = ({}) => (
   <View style={styles.container}>
     <StatusBarView />
     <Post />
     <Text>I'm HomeScreen</Text>
+    <FABComponent />
   </View>
 );
 
